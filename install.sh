6124d3a2725b31a4983f230c28b127c4ebe7e7e0
Robert Gruendler <r.gruendler@gmail.com>
2013-04-15 19:29:17 +0200

    moved repo to external-mirror, added composite repo

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd .. && git clone https://github.com/pulse00/Composer-Eclipse-Plugin.git
-cd Composer-Eclipse-Plugin && git submodule init && git submodule update
-cd .. && git clone https://github.com/pdt-eg/Core-Plugin PEX-Core 
-
